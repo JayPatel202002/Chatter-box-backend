af9cf2f0f863545391b95bde82d589748ebd6038
JayPatel202002 <107167842+JayPatel202002@users.noreply.github.com>
2023-02-01 23:05:09 -0500

    update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/',async(req, res)=>{
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: `${prompt}`,
-        max_tokens: 3000,
+        max_tokens: 4000,
         temperature: 0,
         top_p:1,
         frequency_penaltiy: 0.5,
