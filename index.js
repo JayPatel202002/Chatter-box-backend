c01092285498ae56eefd4fd26c91c6241d1ed04c
JayPatel202002 <107167842+JayPatel202002@users.noreply.github.com>
2023-05-27 08:23:32 -0400

    new

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3080;
+const port = 3080;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 // const dotenv = require('dotenv')
-const { Configuration, OpenAIApi } = require("openai");
-
+const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
-    organization: "org-6xAKrceJfsYwEMY1yK0OH4ua",
+    organization: "org-3xe4LxnmSRYvQhOjEAojZDZp",
     apiKey: process.env.OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/',async(req, res)=>{
-    const prompt = req.body.prompt;
+
+    const { message } = req.body;
+    // console.log(message);
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `${prompt}`,
-        max_tokens: 4000,
-        temperature: 0,
-        top_p:1,
-        frequency_penaltiy: 0.5,
-        presence_penalty: 0
+        prompt: `${message}`,
+        max_tokens: 3000,
+        temperature: 0.5
       });
 
       res.json({
         message: response.data.choices[0].text,
       })
-})
+
+    })
 app.get('/models',async(req, res)=>{
     
-    const response = await openai.listEngines();
-    res.json({
-      models: response.data.data
-    })
-    res.send("success!!");
+  const response = await openai.listEngines();
+  res.json({
+    models: response.data.data
+  })
 })
-
 app.listen(port, ()=>{
     console.log(`The server is running on http://localhost:${port}/`)
 })
